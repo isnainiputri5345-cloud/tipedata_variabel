f2e622fbefe562f7e574b4c7b19434e07a9f4242
isnainiputri5345 <isnainiputri5345@gmail.com>
2026-04-22 10:00:49 +0800

    menambahkan materi3 dan function

---
+++ function.php
@@ -0,0 +1,17 @@
+<?php
+
+function judul($text) {
+    echo "<h2>$text</h2>";
+}
+
+function garis() {
+    echo "<hr>";
+}
+
+function tampilAngka($tipe, $hasil) {
+    echo "<b>$tipe:</b><br>";
+    echo $hasil;
+    echo "<br>";
+}
+
+?>
